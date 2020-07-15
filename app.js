28893b5e6bc57063e76bd1b551c99b2446bd26e9
Mask Wolf <maskwolf2000@gmail.com>
2020-07-16 02:43:18 +0530

    add database

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
+const mongoose=require("mongoose")
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,14 +11,53 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+var items=[];
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true})
+const itemsSchema={
+  task:String
+}
+const Item=mongoose.model("item",itemsSchema)
+const item1=new Item(
+  {
+    task:"Buy Food"
+  }
+)
+const item2=new Item(
+  {
+    task:"Cook Food"
+  }
+)
+const item3=new Item(
+  {
+    task:"Eat Food"
+  }
+)
+const defaultItems=[item1,item2,item3]
+
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+  Item.find({},function(err,item){
+    if(item.length===[]){
+      Item.insertMany(defaultItems,function(err){
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log("Success!")
+        }
+      })
+    }
+    if(err){
+      console.log(err);
+    }
+    else{
+        res.render("list", {listTitle: "Today", newListItems: item});
+    }
+  })
 
-  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
